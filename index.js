2bda2ddc365c921141e71d4b035f2b42439ec166
rabhi alqadi <rabhialqadi22@gmail.com>
2024-05-03 15:05:52 -0700

    update

--- index.js
+++ index.js
@@ -188,7 +188,35 @@ app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
       });
     }
   });
-
+// Update isPinned
+app.put("/update-note-pinned/:noteId", authenticateToken, async (req, res) => {
+    const noteId = req.params.noteId;
+    const { isPinned } = req.body;
+    const { user } = req.user;
+  
+    try {
+      const note = await Note.findOne({ _id: noteId, userId: user._id });
+  
+      if (!note) {
+        return res.status(404).json({ error: true, message: "Note not found" });
+      }
+  
+      note.isPinned = isPinned;
+  
+      await note.save();
+  
+      return res.json({
+        error: false,
+        note,
+        message: "Note pinned status updated successfully",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        error: true,
+        message: "Internal Server Error",
+      });
+    }
+  });
   // Get all Notes
 app.get("/get-all-notes", authenticateToken, async (req, res) => {
     const { user } = req.user;
